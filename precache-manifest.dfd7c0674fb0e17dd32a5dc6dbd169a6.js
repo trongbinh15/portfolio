9d3e22f8d3e1bbbf66fb7bfd072ab23eef427d5b
Trong Binh <trongbinhnguyen15@gmail.com>
2020-08-01 19:02:45 +0700

    Updates

---
+++ precache-manifest.dfd7c0674fb0e17dd32a5dc6dbd169a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0beea817c8749cf7acc14716d8974b5d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2064e2ec28d214cf9d0c",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/portfolio/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2064e2ec28d214cf9d0c",
+    "url": "/portfolio/static/js/main.d9f5d566.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
