51e2d0dd9fcc2d5299e78cfa53eb0fe2ad79e80a
Trong Binh <trongbinhnguyen15@gmail.com>
2020-08-02 14:47:13 +0700

    Updates

---
+++ precache-manifest.5c06a6f7fcd7d9bcf061d1b16ba5c8d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be220f26fe1238f15d659fc960d792dd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a9b41ac0c4f6c00e2ee5",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/portfolio/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b41ac0c4f6c00e2ee5",
+    "url": "/portfolio/static/js/main.503a2e93.chunk.js"
+  },
+  {
+    "revision": "e5793b53fd44ff7c926f",
+    "url": "/portfolio/static/js/runtime-main.6a088b17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.066e0865fd2fc033888d2a7d71d218d6.js"
+  "/portfolio/precache-manifest.5c06a6f7fcd7d9bcf061d1b16ba5c8d2.js"
 );
 
 self.addEventListener('message', (event) => {
