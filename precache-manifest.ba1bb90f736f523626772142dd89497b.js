296063ccd7498fdf76993c3119916d67b4c6bb56
trongbinhnguyen15 <trongbinhnguyen15@gmail.com>
2020-09-04 01:46:19 +0000

    Updates

---
+++ precache-manifest.ba1bb90f736f523626772142dd89497b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "598017eb1daf5d25aa3a607e4b332fb8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "818659ed644f4f98a6eb",
+    "url": "/portfolio/static/css/main.36e38b1b.chunk.css"
+  },
+  {
+    "revision": "edceb4810aae02a446bb",
+    "url": "/portfolio/static/js/2.def5824f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.def5824f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "818659ed644f4f98a6eb",
+    "url": "/portfolio/static/js/main.a4bd84d7.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  }
+]);
