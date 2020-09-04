69c0a95c9e87dbd377223c3e27ac08051b5ad9a2
trongbinhnguyen15 <trongbinhnguyen15@gmail.com>
2020-09-04 04:35:23 +0000

    Updates

--- precache-manifest.979e7a2db7c16f296c300c0f423b5fc3.js
+++ precache-manifest.979e7a2db7c16f296c300c0f423b5fc3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "598017eb1daf5d25aa3a607e4b332fb8",
+    "revision": "172c1b79c81c67b4b99f309c43065700",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "818659ed644f4f98a6eb",
+    "revision": "2bb4a6765e5bd58a9cdb",
     "url": "/portfolio/static/css/main.36e38b1b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.def5824f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "818659ed644f4f98a6eb",
-    "url": "/portfolio/static/js/main.a4bd84d7.chunk.js"
+    "revision": "2bb4a6765e5bd58a9cdb",
+    "url": "/portfolio/static/js/main.7ce4504f.chunk.js"
   },
   {
     "revision": "b37b5bb5a98282a2f0e7",
