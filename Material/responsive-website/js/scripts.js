8edd2fa3eeae5d5e81e91c2649259aa145f0081b
Trong Binh <trongbinhnguyen15@gmail.com>
2020-09-06 15:46:44 +0700

    Education & Works Sections

--- Material/responsive-website/js/scripts.js
+++ Material/responsive-website/js/scripts.js
@@ -10,5 +10,11 @@ $(document).ready(function(){
     $(this).toggleClass("active");
     $(".navbar-menu").toggleClass("active");
   });
+
+  $(".works").magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{enabled:true}
+  });
 });
 
