f04ffe891c1c136a5f3550c289ee6f126a27ca13
Trong Binh <trongbinhnguyen15@gmail.com>
2020-09-06 15:50:34 +0700

    Contact & Footer Section

--- Material/responsive-website/js/scripts.js
+++ Material/responsive-website/js/scripts.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
   $(window).scroll(function(){
-    if(this.scrollY > 20) 
+    if(this.scrollY > 20){
       $(".navbar").addClass("sticky");
-    else
+      $(".goTop").fadeIn();
+    }
+    else{
       $(".navbar").removeClass("sticky");
+      $(".goTop").fadeOut();
+    }
   });
+
+  $(".goTop").click(function(){scroll(0,0)});
 
   $('.menu-toggler').click(function(){
     $(this).toggleClass("active");
