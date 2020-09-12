d20995bd2e094f9a00bdc61a82c1b0dba10b5ea4
trongbinhnguyen15 <trongbinhnguyen15@gmail.com>
2020-09-12 12:44:26 +0000

    [skip ci] Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a942b06b5745eb3cfd9ea35c51f99950.js"
+  "/portfolio/precache-manifest.6fe171df34d80e1dd476702c16dd5cbe.js"
 );
 
 self.addEventListener('message', (event) => {
